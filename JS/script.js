f5f47d8de17afb50c015b5a169864eef1513bd78
pietrolupi <pietro.lupi92@gmail.com>
2023-09-18 14:28:02 +0200

    [edit]:js

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,49 @@
 * (infine lavorerò sulle altre difficoltà regolando anche il numero e le dimensioni dei quadratini generati)
 
 */
+
+const container = document.querySelector('.container-custom')
+
+const diffSelector = document.querySelector('.diff-selector')
+
+
+const startBtn = document.querySelector('.start-btn');
+
+reset(container);
+
+
+startBtn.addEventListener('click',function(){
+  //al click seleziono la difficoltà e la leggo
+  let difficulty = diffSelector.value;
+  console.log(difficulty);
+
+  //creo vari percorsi per ogni difficoltà
+
+  if(difficulty === 'Select difficulty mode:'){
+    alert('SELEZIONA UNA DIFFICOLTA PER PROCEDERE!')
+  }else if(difficulty == 1){
+    for(let i = 1; i <= 100; i++){
+      stampSquare();
+    }
+
+  }else if(difficulty == 2){
+    alert('difficoltà 2')
+  }else{
+    alert('difficoltà 3')
+  }
+
+
+})
+
+//FUNZIONI //////////////////////////////////////////////////
+
+//funzioncina per resettare l'innerhtml di un elemento
+function reset(whatToReset){
+  whatToReset.innerHTML = '';
+}
+//funzione per stampare i quadrati
+function stampSquare(){
+  let square = document.createElement('div');
+  square.classList.add('square')
+  container.append(square);
+}
