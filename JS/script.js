f946dbee812586206af6d10795f86e20c0aefb72
pietrolupi <pietro.lupi92@gmail.com>
2023-09-18 13:53:35 +0200

    [init]:html,css

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+/* 
+* creo su html/css gli elementi con cui lavorare
+* resetto il container principale ad una situazione di default
+* creo tramite js i "quadrati" che andranno a creare la griglia all'interno del container
+(farò un ciclo 100 per situazione standard e 81/49 per le difficoltà superiori)
+* andrò ad aggiungere ai quadrati generati le classi necessarie (per far vedere il numero corrispondente al quadrato al click e colorare la casella di azzurro)
+* inserisco quadrato nel container
+* (infine lavorerò sulle altre difficoltà regolando anche il numero e le dimensioni dei quadratini generati)
+
+*/
